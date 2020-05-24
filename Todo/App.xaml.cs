8908c8539d701007edb7630064d45f8baccd41b2
mephisto <mephisto@skbkontur.ru>
2020-05-24 13:31:27 +0700

    add Todo lib

---
+++ Todo/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Todo.Data;
+using Todo.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+    public partial class App : Application
+    {
+        private static TodoItemDatabase database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            var nav = new NavigationPage(new TodoListPage())
+            {
+                BarBackgroundColor = (Color) Current.Resources["primaryGreen"], BarTextColor = Color.White
+            };
+
+            MainPage = nav;
+        }
+
+        public static TodoItemDatabase Database => database ?? (database = new TodoItemDatabase());
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
+
