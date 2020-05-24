eccb4447796e50da93ecd0191b23e56a2b1b38ed
mephisto <mephisto@skbkontur.ru>
2020-05-24 13:32:59 +0700

    add ItemPage

---
+++ Todo/Views/TodoItemPageCS.cs
@@ -0,0 +1,63 @@
+﻿using Todo.Data;
+using Todo.Models;
+using Xamarin.Forms;
+
+namespace Todo.Views
+{
+    public class TodoItemPageCS : ContentPage
+    {
+        public TodoItemPageCS()
+        {
+            Title = "Todo Item";
+
+            var nameEntry = new Entry();
+            nameEntry.SetBinding(Entry.TextProperty, "Name");
+
+            var notesEntry = new Entry();
+            notesEntry.SetBinding(Entry.TextProperty, "Notes");
+
+            var doneSwitch = new Switch();
+            doneSwitch.SetBinding(Switch.IsToggledProperty, "Done");
+
+            var saveButton = new Button { Text = "Save" };
+            saveButton.Clicked += async (sender, e) =>
+            {
+                var todoItem = (TodoItem)BindingContext;
+                await TodoItemDatabase.SaveItemAsync(todoItem).ConfigureAwait(true);
+                await Navigation.PopAsync().ConfigureAwait(true);
+            };
+
+            var deleteButton = new Button { Text = "Delete" };
+            deleteButton.Clicked += async (sender, e) =>
+            {
+                var todoItem = (TodoItem)BindingContext;
+                await TodoItemDatabase.DeleteItemAsync(todoItem).ConfigureAwait(true);
+                await Navigation.PopAsync().ConfigureAwait(true);
+            };
+
+            var cancelButton = new Button { Text = "Cancel" };
+            cancelButton.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync().ConfigureAwait(true);
+            };
+
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    new Label { Text = "Name" },
+                    nameEntry,
+                    new Label { Text = "Notes" },
+                    notesEntry,
+                    new Label { Text = "Done" },
+                    doneSwitch,
+                    saveButton,
+                    deleteButton,
+                    cancelButton
+                }
+            };
+        }
+    }
+}
